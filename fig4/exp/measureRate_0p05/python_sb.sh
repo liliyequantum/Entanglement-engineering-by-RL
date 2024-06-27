e16f5f77b628b00c3aef1e7fb5c55fd22bf44ab8
liliyequantum <89533667+liliyequantum@users.noreply.github.com>
2024-06-27 16:30:08 -0700

    Add files via upload

---
+++ fig4/exp/measureRate_0p05/python_sb.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH -N 1            # number of nodes
+#SBATCH -c 5            # number of cores 
+#SBATCH -t 3-00:00:00   # time in d-hh:mm:ss
+#SBATCH -p general      # partition 
+#SBATCH -o ./output/slurm.%j.out # file to save job's STDOUT (%j = JobId)
+#SBATCH -e ./output/slurm.%j.err # file to save job's STDERR (%j = JobId)
+#SBATCH --mail-type=ALL # Send an e-mail when a job starts, stops, or fails
+#SBATCH --export=NONE   # Purge the job-submitting shell environment
+
+module purge  # Always purge modules to ensure consistent environments
+# Load required modules for job's environment
+module load mamba/latest
+# Using python, so source activate an appropriate environment
+source activate qutip_RL
+
+python3 trainng_measureRate_repeatIndex_learnRate.py 0.05 5 0.5e-3
+
+source deactivate qutip_RL
---
+++ fig4/exp/measureRate_0p05/training_exp_fig4_data.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 31 15:33:56 2022
+
+@author: LiliYe
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import torch as th
+#https://pytorch.org/get-started/locally/ install pytroch on cuda
+
+import time
+import sys
+
+from stable_baselines3 import PPO
+from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.utils import set_random_seed
+
+from qutip import *
+import os
+
+import numpy as np
+import matplotlib.pyplot as plt
+plt.switch_backend('agg')
+import sys
+
+import gym
+from gym import spaces, logger
+
+from qutip import *
+from numpy import genfromtxt #read excel for parallel env
+from scipy.ndimage import gaussian_filter1d
+
+def update_dir(dirname):
+    if not os.path.exists(dirname):
+      os.mkdir(dirname)
+      print("Folder %s created!" % dirname)
+    else:
+      print("Folder %s already exists" % dirname)
+    
+if __name__ == '__main__':
+    
+    ####### notic for cluster server ################
+    # uncomment plt.switch_backend('agg')
+    
+    ####### cpu or gpu  #######
+    device = 'cpu'#'cuda
+    
+    ####### system parameter  #######
+    measure_rate = 0.05 
+    kappa = 0.01 # decay rate of photon mode
+    mix_p = 0 # mixed prob. of initial state
+    repeat_index = 1
+    
+    ####### training parameter #######
+    n_steps = 500 # times
+    n_update = 5 # update neural network
+    n_envs = 5
+    n_episode = 3000
+    
+    ntraj = 1
+      
+    learning_rate = 1e-3/2
+    n_epochs = 10
+    batch_size = int(n_steps*n_update*n_envs/10)
+    
+    ####### output parameter  #######
+    output_interval = 50 # plot reward...
+    
+    ####### dir  #######
+    rundir = './mix_p_'+str(mix_p)
+    update_dir(rundir)
+    rundir = rundir + '/kappa_'+str(kappa)
+    update_dir(rundir)
+    rundir = rundir + '/measureRate_'+str(measure_rate)
+    update_dir(rundir)
+    rundir = rundir + '/learnRate_'+str(learning_rate)
+    update_dir(rundir)
+    rundir = rundir + '/'+str(repeat_index)
+    update_dir(rundir)
+    
+    dirname = ['dataImp','picture']
+    for i in range(len(dirname)):
+        update_dir(rundir+'/'+dirname[i])
+      
+    ####### env  #######
+    env_kwargs = {'measure_rate':measure_rate,'n_steps':n_steps,'n_envs':n_envs,\
+                  'n_episode':n_episode,'ntraj':ntraj,\
+                   'output_interval':output_interval,'rundir':rundir,\
+                   'kappa':kappa,'mix_p':mix_p}
+        
+    env_id = "quantumEnvExp_update-v0"
+    env = make_vec_env(env_id, n_envs=n_envs, vec_env_cls=SubprocVecEnv,monitor_dir=rundir+'/monitor',env_kwargs=env_kwargs)
+    
+    ####### PPO  #######
+    policy_kwargs = dict(activation_fn=th.nn.ReLU,\
+                          net_arch=[dict(vf=[256, 128, 64], pi=[256, 128, 64])])
+    #policy_kwargs = dict(activation_fn=th.nn.ReLU,
+                         #net_arch=[256,dict(pi=[128, 64], vf=[128, 64])])    
+    model = PPO("MlpPolicy", env,policy_kwargs = policy_kwargs,\
+                learning_rate=learning_rate, n_steps = n_steps*n_update, \
+                    batch_size = batch_size, n_epochs = n_epochs,\
+                    verbose=1,device=device)
+    
+    start = time.time()
+    model.learn(total_timesteps = n_steps * n_episode * n_envs)
+    end = time.time()
+    print('training time elapsed: ',end-start)
+                
+    model.save(rundir+"/ppo_quantumOptomechanics")
+    model.save(rundir+"/dataImp/ppo_quantumOptomechanics")
+    del model    
+    
+    sys.exit()
